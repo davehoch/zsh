4c3fff3614f9112712d0483168ed9cc555bff2dc
David Hochhauser <dhochhauser@clearwateranalytics.com>
2022-06-27 15:13:05 -0600

    adding HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK option

--- env.zsh
+++ env.zsh
@@ -1,6 +1,9 @@
 # Put gui applications in personal Applications folder.  It doesn't need sudo permission.
 export HOMEBREW_CASK_OPTS="--appdir='$HOME/Applications' --require-sha"
 
+# only upgrade the things I asked to upgrade, not all dependencies as well
+export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+
 export BROWSER=open
 export EDITOR=code
 
