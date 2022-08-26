e6afd05d03cc46d7f7755b08bfc051df0985dfb6
David Hochhauser <dhochhauser@clearwateranalytics.com>
2022-08-26 17:04:32 -0600

    added AUTO_PUSHD to zsh options

--- options.zsh
+++ options.zsh
@@ -1,8 +1,13 @@
-# History file ~/.zsh_history
-# The meaning of these options can be found in man page of `zshoptions`.
-# setopt NO_CASE_GLOB # case insensitive globbing.  i.e D* will match d*
-# setopt CORRECT  # enables auto correct.  It turns out that I don't like the autocorrect
-# setopt CORRECT_ALL
-setopt HIST_IGNORE_ALL_DUPS  # do not put duplicated command into history list
-setopt HIST_REDUCE_BLANKS  # removes blank lines from history
-setopt HIST_SAVE_NO_DUPS  # do not save duplicated command
+# Documentation for options
+# https://zsh.sourceforge.io/Doc/Release/Options.html
+
+setopt AUTO_PUSHD               # Make cd push the old directory onto the directory stack.
+setopt NUMERIC_GLOB_SORT        # If numeric filenames are matched by a filename generation pattern, sort the filenames numerically rather than lexicographically.
+# setopt NO_CASE_GLOB           # case insensitive globbing.  i.e D* will match d*
+
+
+setopt HIST_IGNORE_ALL_DUPS     # do not put duplicated command into history list
+setopt HIST_IGNORE_SPACE        # Remove command lines from the history list when the first character on the line is a space, or when one of the expanded aliases contains a leading space
+setopt HIST_REDUCE_BLANKS       # removes blank lines from history
+setopt HIST_SAVE_NO_DUPS        # do not save duplicated command
+
