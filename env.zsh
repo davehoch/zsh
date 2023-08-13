c52fa30bc0b12d216d7d93860e1b52bb7f0060c3
David Hochhauser <dhochhauser@clearwateranalytics.com>
2023-08-13 08:23:24 -0600

    switching less to use highlight instead of source-highlight

--- env.zsh
+++ env.zsh
@@ -12,6 +12,14 @@ export EDITOR=code
 
 # less syntax highlighting + source-highlight installation
 # https://www.gnu.org/software/src-highlite/
-# This also looks promising: http://www.andre-simon.de/doku/highlight/en/highlight.php
-export LESSOPEN="| /usr/local/bin/src-hilite-lesspipe.sh %s"
-export LESS=' --RAW-CONTROL-CHARS --tabs=4'
+# export LESSOPEN="| /usr/local/bin/src-hilite-lesspipe.sh %s"
+# export LESS=' --RAW-CONTROL-CHARS --tabs=4'
+# export LESS=' --tabs=4'
+
+# Pipe Highlight to less
+export LESSOPEN="| highlight %s --out-format xterm256 --force -s edit-xcode --no-trailing-nl"
+export LESS=" -R"
+alias less='less --long-prompt --line-numbers --hilite-search --status-column --ignore-case --underline-special --quit-if-one-screen --tabs=4'
+
+# Use "highlight" in place of "cat"
+# alias cat="highlight $1 --out-format xterm256 -l --force -s edit-xcodet --no-trailing-nl"
