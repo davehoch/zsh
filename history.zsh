63b00b00558a9370a56386de112058a8acc89912
David Hochhauser <daveh@csd.net>
2024-04-17 21:04:23 -0600

    .

--- history.zsh
+++ history.zsh
@@ -1,8 +1,17 @@
+# https://zsh.sourceforge.io/Doc/Release/Options.html (16.2.4 History)
+
 HISTFILE=~/.config/zsh/.zsh_history
 HISTSIZE=1000
 SAVEHIST=1000
 
-setopt HIST_IGNORE_ALL_DUPS     # Do not enter command lines into the history list if they are duplicates of the previous event.
-setopt HIST_IGNORE_SPACE        # Remove command lines from the history list when the first character on the line is a space, or when one of the expanded aliases contains a leading space
-setopt HIST_REDUCE_BLANKS       # removes blank lines from history
-setopt HIST_SAVE_NO_DUPS        # When writing out the history file, older commands that duplicate newer ones are omitted.
+# setopt EXTENDED_HISTORY      # Write the history file in the ':start:elapsed;command' format.
+# setopt INC_APPEND_HISTORY    # Write to the history file immediately, not when the shell exits.
+# setopt SHARE_HISTORY         # Share history between all sessions.
+# setopt HIST_IGNORE_DUPS      # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS  # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_IGNORE_SPACE     # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS     # Do not write a duplicate event to the history file.
+# setopt HIST_VERIFY           # Do not execute immediately upon history expansion.
+# setopt APPEND_HISTORY        # append to history file (Default)
+# setopt HIST_NO_STORE         # Don't store history commands
+setopt HIST_REDUCE_BLANKS    # Remove superfluous blanks from each command line being added to the history.
